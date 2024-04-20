453f64378ce02d5b348865f0ebac70c199efcdb4
Moe Nakamura <moe.aow124@gmail.com>
2024-04-20 19:30:27 +0900

    LLMメッセージ　表示を男女で分岐
    フキダシや画像を変更

--- front/src/app/_components/TodayMessageBannar.tsx
+++ front/src/app/_components/TodayMessageBannar.tsx
@@ -1,70 +1,56 @@
 "use client";
-import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { useState, useEffect, useRef } from "react";
 import styles from "./banner.module.css";
 
-export default function MessageBannar({ id }: { id: string }) {
-  const pathname = usePathname(); // usePathnameフックを使用
+const IMAGE_URL = "/img/dog.png";
+
+export default function TodayMessageBannar() {
+    const router = useRouter();
   const [message, setMessage] = useState("");
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [showBanner, setShowBanner] = useState(false); // バナー表示の状態を管理
   const [bannerStyle, setBannerStyle] = useState(styles.notificationBanner);
-  const [imageURL, setImageURL] = useState('/img/dog.png'); 
   const bannerRef = useRef(null);
 
   //idが変更されたときにfetchMessageを呼び出す
   useEffect(() => {
     //バックエンドにイベントIDを送信し、生成されたメッセージを取得する関数
-    const fetchMessage = async () => {
-      if (!id) return; // idがない場合は処理をスキップ
-
+    const fetchTodaysMessage = async () => {
       setIsLoading(true);
       setError(null);
 
       try {
-        //メッセージ生成APIへのリクエスト
-        const response = await fetch(`/api/events/${id}/generate-message`, {
+        //メッセージ生成APIへのリクエスト　"/api/events/today" `/api/events/${id}/generate-message`
+        const response = await fetch("/api/events/today" , {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
           },
         });
 
-        console.log(`Response status: ${response.status}`);
+        console.log(`Today Response status: ${response.status}`);
 
         if (!response.ok) {
-          throw new Error("Failed to generate message");
+          throw new Error("Failed to generate today's message");
         }
 
         // レスポンスからJSONデータを取得
         const data = await response.json();
-        console.log("Received data:", data);
+        console.log("Today Received data:", data);
          //取得したメッセージ不要な引用符を削除
         setMessage(data.message.replace(/「/g, "").replace(/」/g, ""));
         setShowBanner(true); // バナーを表示
       } catch (error: any) {
-        console.error("Error:", error);
+        console.error("Failed to load today's message:", error);
         setError(error.message);
       } finally {
         setIsLoading(false);
       }
     };
-    fetchMessage(); //関数を呼び出し
-  }, [id]); //idが変更された時にuseEffectを実行
-
-  //ユーザーとパートナーでフキダシ色（CSS）・画像変更
-  useEffect(() => {
-    if  (pathname === "/calendar") { //ユーザー用カレンダーなら
-      setBannerStyle(`${styles.notificationBanner} ${styles.userBanner}`);
-      setImageURL("/img/dog.png"); // 画像URL
-    } else if (pathname === "/partner") { //パートナー用カレンダーなら
-      setBannerStyle(`${styles.notificationBanner} ${styles.partnerBanner}`);
-      setImageURL("/img/lady.png"); // 画像URL
-    } else {
-      setBannerStyle(styles.notificationBanner);
-    }
-  }, [pathname]);
+    fetchTodaysMessage(); //関数を呼び出し
+  }, []); 
 
 
   const handleClose = () => {
@@ -83,14 +69,14 @@ export default function MessageBannar({ id }: { id: string }) {
         <div className={styles.notificationContainer}>
           <div
             id="notification-banner"
-            className={`${bannerStyle} ${styles.fadeup}`}
+            className={`${styles.notificationBanner} ${styles.fadeup}`}
             ref={bannerRef}
           >
             <button className={styles.closeButton} onClick={handleClose}>×</button>
             {message}
             <img
-              src={imageURL} // 状態から画像のURLを設定
-              alt="Commenting icon"
+              src={IMAGE_URL}
+              alt="Commenting Dog"
               className={`${styles.float}`}
             />
           </div>
